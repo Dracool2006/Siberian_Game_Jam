eeea13cfa1a5617f5b36befe6bca2c4a1c5c0a00
HgCl2 <s.almas_99@mail.ru>
2021-04-03 13:50:51 +0600

    Движение камеры(исправленное)

--- Siberian_Game_Jam/Assets/Scripts/CameraMovement.cs
+++ Siberian_Game_Jam/Assets/Scripts/CameraMovement.cs
@@ -29,8 +29,7 @@ public class CameraMovement : MonoBehaviour
         Vector3 playerPosition = player.transform.position;
         Vector2 topRightCorner = new Vector2(9f / 2 + cameraPosition.x, 5f / 2 + cameraPosition.y);
         Vector2 downLeftCorner = new Vector2(cameraPosition.x - 9f / 2, cameraPosition.y - 5f / 2);
-        print(topRightCorner);
-        print(downLeftCorner);
+        
         if (player)
         {
             if(playerPosition.x > topRightCorner.x)
@@ -52,13 +51,7 @@ public class CameraMovement : MonoBehaviour
 
             cameraPosition = Vector3.Lerp(transform.position, cameraPosition, damping * Time.deltaTime);
             transform.position = cameraPosition;
-            /*Vector2 sideOfMove = SideOfMovement(Angle());
-            Vector3 target;
-            if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
-                target = new Vector3(player.transform.position.x + sideOfMove.x * offset.x, player.transform.position.y + sideOfMove.y * offset.y, transform.position.z);
-                Vector3 currentPosition = Vector3.Lerp(transform.position, target, damping * Time.deltaTime);
-                transform.position = currentPosition;
-            }*/
+           
         }
     }
 }
