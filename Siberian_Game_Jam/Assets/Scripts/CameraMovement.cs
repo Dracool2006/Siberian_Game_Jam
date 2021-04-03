7dd0de47971c04384beaec5b996df9fc7999b56a
HgCl2 <s.almas_99@mail.ru>
2021-04-03 11:39:11 +0600

    Движение камеры(исправленная)

--- Siberian_Game_Jam/Assets/Scripts/CameraMovement.cs
+++ Siberian_Game_Jam/Assets/Scripts/CameraMovement.cs
@@ -7,7 +7,7 @@ public class CameraMovement : MonoBehaviour
 
     public GameObject player;
     public Vector2 offset = new Vector2(1f, 1f);
-    public float damping = 1f;
+    public float damping = 1.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,68 +21,44 @@ public class CameraMovement : MonoBehaviour
           FollowCamera();
     }
 
-    float Angle()
-    {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 lookDirection = mousePos - (Vector2)player.transform.position;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
-        return angle;
-    }
-
-    Vector2 SideOfMovement(float angle)
-    {
-        //Vector2(x, y)
-        if (angle > -20 && angle < 20)
-        {
-            return new Vector2(0, 1);
-        }
-        else if (angle > -65 && angle < 25)
-        {
-            return new Vector2(1, 1);
-        }
-        else if (angle > -110 && angle < -70)
-        {
-            return new Vector2(1, 0);
-        }
-        else if (angle > -155 && angle < -115)
-        {
-            return new Vector2(1, -1);
-        }
-        else if (angle > -200 && angle < -160)
-        {
-            return new Vector2(0, -1);
-        }
-        else if (angle > -245 && angle < -205)
-        {
-            return new Vector2(-1, -1);
-        }
-        else if ((angle > -270 && angle < -250) || (angle > 70 && angle < 90))
-        {
-            return new Vector2(-1, 0);
-        }
-        else if (angle > 25 && angle < 65)
-        {
-            return new Vector2(-1, 1);
-        }
-
-        return new Vector2(0, 0);
-    }
+    
 
     void FollowCamera()
     {
         Vector3 cameraPosition = transform.position;
         Vector3 playerPosition = player.transform.position;
-
-        if(player)
+        Vector2 topRightCorner = new Vector2(9f / 2 + cameraPosition.x, 5f / 2 + cameraPosition.y);
+        Vector2 downLeftCorner = new Vector2(cameraPosition.x - 9f / 2, cameraPosition.y - 5f / 2);
+        print(topRightCorner);
+        print(downLeftCorner);
+        if (player)
         {
-            
-            Vector2 sideOfMove = SideOfMovement(Angle());
+            if(playerPosition.x > topRightCorner.x)
+            {
+                cameraPosition.x += offset.x;
+            }
+            if (playerPosition.y > topRightCorner.y)
+            {
+                cameraPosition.y += offset.y;
+            }
+            if (playerPosition.x < downLeftCorner.x)
+            {
+                cameraPosition.x -= offset.x;
+            }
+            if (playerPosition.y < downLeftCorner.y)
+            {
+                cameraPosition.y -= offset.y;
+            }
+
+            cameraPosition = Vector3.Lerp(transform.position, cameraPosition, damping * Time.deltaTime);
+            transform.position = cameraPosition;
+            /*Vector2 sideOfMove = SideOfMovement(Angle());
             Vector3 target;
             if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
                 target = new Vector3(player.transform.position.x + sideOfMove.x * offset.x, player.transform.position.y + sideOfMove.y * offset.y, transform.position.z);
                 Vector3 currentPosition = Vector3.Lerp(transform.position, target, damping * Time.deltaTime);
                 transform.position = currentPosition;
-            }
+            }*/
         }
     }
 }
